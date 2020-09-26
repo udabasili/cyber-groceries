c5ddd65e21b07a550df06546d22acf496d2b950b
udabasili <udbasili@yahoo.com>
2020-09-26 13:35:34 -0300

    major project update

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,9 +5,7 @@ import { persistStore } from "redux-persist";
 
 const middleware = [thunk]
 
-const composeEnhancers = process.env.NODE_ENV === 'production' ?
-    compose :
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 export const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(...middleware)
