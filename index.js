5981d4d6d40a06b4b5414ac9adb85731752a0f91
udabasili <udbasili@yahoo.com>
2020-10-27 11:21:34 -0300

    update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(helmet.dnsPrefetchControl());
 app.use(helmet.expectCt());
 app.use(helmet.frameguard());
 app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
 app.use(helmet.permittedCrossDomainPolicies());
